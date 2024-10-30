b3ba26a609b9f264b6bd71d7ee47667aa27b931f
Fangsangik <hwangsangik@gmail.com>
2024-10-30 21:20:30 +0900

    feat = "Schedule에서 비밀번호 validation Method"

--- src/main/java/com/example/dailyschedule/domain/Schedule.java
+++ src/main/java/com/example/dailyschedule/domain/Schedule.java
@@ -1,11 +1,13 @@
 package com.example.dailyschedule.domain;
 
+import com.example.dailyschedule.dto.ScheduleDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
@@ -17,7 +19,34 @@ public class Schedule {
     private String author; //작성자
     private String password; //비말번호
     private String description; //상세 내용
-    private LocalDate createdAt; //생성 날짜
-    private LocalDate updatedAt; //수정 날짜
-    private LocalDate deletedAt; //삭제 날짜
+    private LocalDateTime createdAt; //생성 날짜
+    private LocalDateTime updatedAt; //수정 날짜
+    private LocalDateTime deletedAt; //삭제 날짜
+
+    //password 로직 검증
+    public Schedule updateByScheduleDto(ScheduleDto scheduleDto) {
+        if (!scheduleDto.getPassword().equals(password)) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+        this.title = scheduleDto.getTitle();
+        this.author = scheduleDto.getAuthor();
+        this.updatedAt = LocalDateTime.now();
+
+        return this;
+    }
+
+    public Schedule deleteByScheduleById(ScheduleDto scheduleDto) {
+        if (!scheduleDto.getPassword().equals(password)) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+        if (!scheduleDto.getId().equals(id)) {
+            throw new IllegalArgumentException("아이디가 일치하지 않습니다.");
+        }
+
+        this.deletedAt = LocalDateTime.now();
+
+        return this;
+    }
 }
