222f83d39a8e833d1b42695d1f498bb5b0ed72a5
Fangsangik <hwangsangik@gmail.com>
2024-11-01 10:04:03 +0900

    feat : controller

--- src/main/java/com/example/dailyschedule/schedule/controller/ScheduleController.java
+++ src/main/java/com/example/dailyschedule/schedule/controller/ScheduleController.java
@@ -1,4 +1,13 @@
 package com.example.dailyschedule.schedule.controller;
 
+import com.example.dailyschedule.schedule.dto.ScheduleDto;
+import com.example.dailyschedule.schedule.service.ScheduleServiceImpl;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/schedules")
+@RestController
 public class ScheduleController {
 }
