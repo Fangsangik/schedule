fdfeb445aaac93f80fec4900300c55ca39528385
Fangsangik <hwangsangik@gmail.com>
2024-11-04 20:20:39 +0900

    feat : "valid 추가"

--- src/main/java/com/example/dailyschedule/schedule/dto/DeleteScheduleRequest.java
+++ src/main/java/com/example/dailyschedule/schedule/dto/DeleteScheduleRequest.java
@@ -1,9 +1,11 @@
 package com.example.dailyschedule.schedule.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class DeleteScheduleRequest {
     private Long id;
+    @NotBlank(message = "비밀번호를 입력해 주세요")
     private String password;
 }
