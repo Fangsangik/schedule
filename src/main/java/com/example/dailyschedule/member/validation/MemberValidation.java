8e8a4040c83a2e6cb1b5ee6c58ea6c71bb4d2b5b
Fangsangik <hwangsangik@gmail.com>
2024-11-04 13:08:17 +0900

    feat : "MemberValidation" - Error 상태코드 추가

--- src/main/java/com/example/dailyschedule/member/validation/MemberValidation.java
+++ src/main/java/com/example/dailyschedule/member/validation/MemberValidation.java
@@ -1,12 +1,17 @@
 package com.example.dailyschedule.member.validation;
 
+import com.example.dailyschedule.error.CustomException;
+import com.example.dailyschedule.error.type.ErrorCode;
 import com.example.dailyschedule.member.dto.MemberDto;
 import com.example.dailyschedule.member.entity.Member;
 import com.example.dailyschedule.member.repository.MemberRepository;
 
+import static com.example.dailyschedule.error.type.ErrorCode.*;
+
 public class MemberValidation {
 
     private final MemberRepository memberRepository;
+
 
     public MemberValidation(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
@@ -16,14 +21,14 @@ public class MemberValidation {
     public void validateDuplicateUserId(String userId) {
         Member existingMember = memberRepository.findByUserId(userId);
         if (existingMember != null) {
-            throw new IllegalArgumentException("중복된 userId입니다.");
+            throw new CustomException(USER_ID_EXIST);
         }
     }
 
     public Member validateExistId(Long id) {
         Member existingMember = memberRepository.findById(id);
         if (existingMember == null) {
-            throw new IllegalArgumentException("ID 값이 없습니다.");
+            throw new CustomException(ID_EXIST);
         }
 
         return existingMember;
@@ -32,23 +37,23 @@ public class MemberValidation {
     public Member findByUserId(String userId) {
         Member member = memberRepository.findByUserId(userId);
         if (member == null) {
-            throw new IllegalArgumentException("해당 ID 회원이 존재하지 않습니다.");
+            throw new CustomException(ID_NOT_FOUND);
         }
         return member;
     }
 
     public void validatePassword(Member member, MemberDto memberDto) {
         if (!member.getPassword().equals(memberDto.getPassword())) {
-            throw new IllegalArgumentException("비밀번호가 맞지 않습니다.");
+            throw new CustomException(PASSWORD_INCORRECT);
         }
     }
 
     public Member findByName(String name) {
         Member memberName = memberRepository.findByName(name);
         if (memberName == null) {
-            throw new IllegalArgumentException("해당 이름이 없습니다.");
+            throw new CustomException(NOT_FOUND);
         }
 
         return memberName;
     }
- }
+}
