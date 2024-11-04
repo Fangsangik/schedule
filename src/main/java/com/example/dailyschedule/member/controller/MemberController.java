fb93db631765572d7ef5c8a7d38f0114736a083b
Fangsangik <hwangsangik@gmail.com>
2024-11-04 20:23:37 +0900

    refactor : "MemberController" - ErrorCode 넣음

--- src/main/java/com/example/dailyschedule/member/controller/MemberController.java
+++ src/main/java/com/example/dailyschedule/member/controller/MemberController.java
@@ -2,9 +2,12 @@ package com.example.dailyschedule.member.controller;
 
 import com.example.dailyschedule.error.CustomException;
 import com.example.dailyschedule.error.type.ErrorCode;
+import com.example.dailyschedule.member.dto.DeleteMemberRequest;
 import com.example.dailyschedule.member.dto.MemberDto;
 import com.example.dailyschedule.member.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,7 +29,7 @@ public class MemberController {
         try {
             MemberDto findMember = memberService.findById(memberId);
             return ResponseEntity.ok(findMember);
-        } catch (IllegalArgumentException e) {
+        } catch (CustomException e) {
             log.error("해당 회원을 찾을 수 없습니다. {}", e.getMessage());
             throw new CustomException(ID_NOT_FOUND);
         }
@@ -37,42 +40,43 @@ public class MemberController {
         try {
             MemberDto findMember = memberService.findByUserId(userId);
             return ResponseEntity.ok(findMember);
-        } catch (IllegalArgumentException e) {
+        } catch (CustomException e) {
             log.error("해당 회원을 찾을 수 없습니다. {}", e.getMessage());
-            throw new CustomException(ErrorCode.NOT_FOUND);
+            throw new CustomException(INVALID_MEMBER_INFO);
         }
     }
 
     @PostMapping("/")
-    public ResponseEntity<?> createMember(@RequestBody MemberDto memberDto) {
+    public ResponseEntity<?> createMember(@Valid @RequestBody MemberDto memberDto) {
         try {
             MemberDto createMember = memberService.createMember(memberDto);
-            return ResponseEntity.ok(createMember);
-        } catch (IllegalArgumentException e) {
-            log.error("회원 생성하는데 실패했습니다. {}", e.getMessage());
-            throw new CustomException(CREATION_FAILED);
+            return ResponseEntity.status(HttpStatus.CREATED).body(createMember);
+        } catch (CustomException e) {
+            log.error("일정을 생성하는데 실패했습니다: {}", e.getMessage(), e);
+            throw new CustomException(ErrorCode.CREATION_FAILED);
         }
     }
 
     @PutMapping("/{memberId}")
-    public ResponseEntity<?> updateMember(@PathVariable("memberId") Long memberId, @RequestBody MemberDto memberDto) {
+    public ResponseEntity<?> updateMember(@PathVariable("memberId") Long memberId, @Valid @RequestBody MemberDto memberDto) {
         try {
             MemberDto updatedMember = memberService.updateMember(memberId, memberDto);
             return ResponseEntity.ok(updatedMember);
-        } catch (IllegalArgumentException e) {
-            log.error("회원 update 하는데 실패했습니다. {}", e.getMessage());
-            throw new CustomException(UPDATE_FAILED);
+        } catch (CustomException e) {
+            log.error("회원 업데이트에 실패했습니다. {}", e.getMessage());
+            throw e;
         }
     }
 
     @DeleteMapping("/{memberId}")
-    public ResponseEntity<?> deleteMember(@PathVariable("memberId") Long memberId, MemberDto memberDto) {
+    public ResponseEntity<?> deleteMember(@PathVariable("memberId") Long memberId, @Valid @RequestBody DeleteMemberRequest deleteMemberRequest) {
         try {
-            MemberDto deleteMember = memberService.deleteMember(memberId, memberDto);
-            return ResponseEntity.ok(deleteMember);
-        } catch (IllegalArgumentException e) {
+            MemberDto deletedMember = memberService.deleteMember(memberId, deleteMemberRequest.getPassword());
+            return ResponseEntity.ok(deletedMember);
+        } catch (CustomException e) {
             log.error("회원을 삭제하는데 실패했습니다. {}", e.getMessage());
             throw new CustomException(DELETE_FAILED);
         }
     }
+
 }
