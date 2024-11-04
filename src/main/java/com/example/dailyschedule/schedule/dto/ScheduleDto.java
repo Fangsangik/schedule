181fa78bd984a2f4a4dce687c958816c0d6ef5ad
Fangsangik <hwangsangik@gmail.com>
2024-11-04 09:26:00 +0900

    refactor : "ScheduleDto" - JsonFormat patter 수정

--- src/main/java/com/example/dailyschedule/schedule/dto/ScheduleDto.java
+++ src/main/java/com/example/dailyschedule/schedule/dto/ScheduleDto.java
@@ -1,12 +1,10 @@
 package com.example.dailyschedule.schedule.dto;
 
 import com.example.dailyschedule.member.dto.MemberDto;
-import com.example.dailyschedule.member.entity.Member;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
@@ -17,11 +15,11 @@ public class ScheduleDto {
     private String author; //작성자
     private String password; //비말번호
     private String description; //상세 내용
-    @JsonFormat(pattern = "HH:MM")
+    @JsonFormat(pattern = "yyyy:MM:dd:HH:mm")
     private LocalDateTime createdAt; //생성 날짜
-    @JsonFormat(pattern = "HH:MM")
+    @JsonFormat(pattern = "yyyy:MM:dd:HH:mm")
     private LocalDateTime updatedAt; //수정 날짜
-    @JsonFormat(pattern = "HH:MM")
+    @JsonFormat(pattern = "yyyy:MM:dd:HH:mm")
     private LocalDateTime deletedAt; //삭제 날짜
     private MemberDto memberDto;
 }
