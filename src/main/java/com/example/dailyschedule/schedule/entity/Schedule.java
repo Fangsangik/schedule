2440f00e5cfba19f2ab2b3478358b846937ca483
Fangsangik <hwangsangik@gmail.com>
2024-11-04 09:25:01 +0900

    refactor : "Schedule" - JsomFormat 수정

--- src/main/java/com/example/dailyschedule/schedule/entity/Schedule.java
+++ src/main/java/com/example/dailyschedule/schedule/entity/Schedule.java
@@ -1,7 +1,6 @@
 package com.example.dailyschedule.schedule.entity;
 
 import com.example.dailyschedule.member.entity.Member;
-import com.example.dailyschedule.schedule.dto.ScheduleDto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -20,11 +19,11 @@ public class Schedule {
     private String author; //작성자
     private String password; //비말번호
     private String description; //상세 내용
-    @JsonFormat(pattern = "HH:mm")
+    @JsonFormat(pattern = "yyyy:MM:dd:HH:mm")
     private LocalDateTime createdAt; //생성 날짜
-    @JsonFormat(pattern = "HH:mm")
+    @JsonFormat(pattern = "yyyy:MM:dd:HH:mm")
     private LocalDateTime updatedAt; //수정 날짜
-    @JsonFormat(pattern = "HH:mm")
+    @JsonFormat(pattern = "yyyy:MM:dd:HH:mm")
     private LocalDateTime deletedAt; //삭제 날짜
     private Member member;
 }
