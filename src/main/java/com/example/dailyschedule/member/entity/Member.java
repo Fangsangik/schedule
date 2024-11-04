83ea8a748be68caa6b884fdc4903cd7944a4f722
Fangsangik <hwangsangik@gmail.com>
2024-11-04 13:05:30 +0900

    refactor : "Member" - toBuilder pattern 적용

--- src/main/java/com/example/dailyschedule/member/entity/Member.java
+++ src/main/java/com/example/dailyschedule/member/entity/Member.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+@Builder(toBuilder = true)
 public class Member {
     private Long id;
     private String userId;
