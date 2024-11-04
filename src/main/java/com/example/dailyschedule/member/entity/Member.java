f48572e6b9cc0710fbc315537d559f48a50a4371
Fangsangik <hwangsangik@gmail.com>
2024-11-04 20:22:02 +0900

    feat : "JsonFormatPatter 변경"

--- src/main/java/com/example/dailyschedule/member/entity/Member.java
+++ src/main/java/com/example/dailyschedule/member/entity/Member.java
@@ -1,6 +1,7 @@
 package com.example.dailyschedule.member.entity;
 
 import com.example.dailyschedule.member.dto.MemberDto;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -18,5 +19,6 @@ public class Member {
     private String password;
     private String name;
     private String email;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime updatedAt;
 }
