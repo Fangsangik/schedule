e83af245f6ed763f5c763568de21ee05bbae8814
Fangsangik <hwangsangik@gmail.com>
2024-11-04 20:24:03 +0900

    refactor : "MemberDto" - validation 넣음

--- src/main/java/com/example/dailyschedule/member/dto/MemberDto.java
+++ src/main/java/com/example/dailyschedule/member/dto/MemberDto.java
@@ -1,21 +1,34 @@
 package com.example.dailyschedule.member.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
-@Getter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class MemberDto {
+    @NotNull(message = "ID는 필수 항목입니다.")
     private Long id;
+
+    @NotBlank(message = "사용자 ID는 필수 항목입니다.")
     private String userId;
+
+    @NotBlank(message = "비밀번호는 필수 항목입니다.")
     private String password;
+
+    @NotBlank(message = "이름은 필수 항목입니다.")
     private String name;
+
+    @NotBlank(message = "이메일은 필수 항목입니다.")
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
     private String email;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime updatedAt;
 }
