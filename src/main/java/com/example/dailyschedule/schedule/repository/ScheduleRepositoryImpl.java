44886ca3b92f697a3fd68aeaded19a34d8b84ec0
Fangsangik <hwangsangik@gmail.com>
2024-11-04 09:28:51 +0900

    feat : "ScheduleRepositoryImpl" - findSchedulesByUpdatedDateAndAuthor : 스케줄 Id와 회원 Id 동시 조회

--- src/main/java/com/example/dailyschedule/schedule/repository/ScheduleRepositoryImpl.java
+++ src/main/java/com/example/dailyschedule/schedule/repository/ScheduleRepositoryImpl.java
@@ -116,15 +116,30 @@ public class ScheduleRepositoryImpl {
     }
 
 
-    public Schedule findByDate(LocalDateTime date) {
+
+
+    public List<Schedule> findSchedulesByUpdatedDateAndAuthor(LocalDateTime updatedAt, String author) {
+        if (updatedAt == null && author == null) {
+            throw new IllegalArgumentException("해당 이름으로 수정된 날짜를 찾을 수 없습니다.");
+        }
+
+        String sql = "SELECT * FROM schedule WHERE (updated_at = ? OR ? IS NULL) OR (author = ? OR ? IS NULL) ORDER BY updated_at DESC";
+
+        return jdbcTemplate.query(sql, new Object[]{updatedAt, updatedAt, author, author}, scheduleRowMapper());
+    }
+
+
+    public List<Schedule> findByDate(LocalDateTime date) {
         if (date == null) {
             throw new IllegalArgumentException("해당 날짜가 없습니다.");
         }
-        String sql = "SELECT * FROM schedule " +
-                "WHERE created_at = ? OR updated_at = ? OR deleted_at = ?";
 
-        return jdbcTemplate.queryForObject(sql, new Object[]{date, date, date}, scheduleRowMapper());
+        String sql = "SELECT * FROM schedule WHERE created_at = ? OR updated_at = ? OR deleted_at = ?";
+
+        // query 메서드를 사용하여 다수의 결과를 리스트로 반환
+        return jdbcTemplate.query(sql, new Object[]{date, date, date}, scheduleRowMapper());
     }
+
 
 
     public List<Schedule> findSchedulesByMemberId(Long memberId) {
