a285f78271b13480edd120b2c64f770d1f21a8aa
Fangsangik <hwangsangik@gmail.com>
2024-11-05 18:01:19 +0900

    refactor : "ScheduleConverter" - member 추가

--- src/main/java/com/example/dailyschedule/schedule/converter/ScheduleConverter.java
+++ src/main/java/com/example/dailyschedule/schedule/converter/ScheduleConverter.java
@@ -1,5 +1,9 @@
 package com.example.dailyschedule.schedule.converter;
 
+import com.example.dailyschedule.error.CustomException;
+import com.example.dailyschedule.error.type.ErrorCode;
+import com.example.dailyschedule.member.converter.MemberConverter;
+import com.example.dailyschedule.member.entity.Member;
 import com.example.dailyschedule.schedule.entity.Schedule;
 import com.example.dailyschedule.schedule.dto.ScheduleDto;
 import org.springframework.stereotype.Component;
@@ -7,8 +11,32 @@ import org.springframework.stereotype.Component;
 @Component
 public class ScheduleConverter {
 
-    public Schedule toEntity(ScheduleDto scheduleDto) {
+    private final MemberConverter memberConverter;
 
+    public ScheduleConverter(MemberConverter memberConverter) {
+        this.memberConverter = memberConverter;
+    }
+
+    // Member가 포함된 Schedule 생성
+    public Schedule toEntityIncludeMember(ScheduleDto scheduleDto, Member member) {
+        if (member == null) {
+            throw new CustomException(ErrorCode.INVALID_MEMBER_INFO); // member가 null일 때 예외 처리
+        }
+
+        return Schedule.builder()
+                .id(scheduleDto.getId())
+                .author(scheduleDto.getAuthor())
+                .title(scheduleDto.getTitle())
+                .createdAt(scheduleDto.getCreatedAt())
+                .deletedAt(scheduleDto.getDeletedAt())
+                .description(scheduleDto.getDescription())
+                .updatedAt(scheduleDto.getUpdatedAt())
+                .password(scheduleDto.getPassword())
+                .member(member) // Member 포함
+                .build();
+    }
+
+    public Schedule toEntityWithoutMember(ScheduleDto scheduleDto) {
         return Schedule.builder()
                 .id(scheduleDto.getId())
                 .author(scheduleDto.getAuthor())
@@ -32,6 +60,8 @@ public class ScheduleConverter {
                 .deletedAt(schedule.getDeletedAt())
                 .description(schedule.getDescription())
                 .password(schedule.getPassword())
+                .memberDto(schedule.getMember() != null ? memberConverter.toDto(schedule.getMember()) : null)
                 .build();
     }
+
 }
