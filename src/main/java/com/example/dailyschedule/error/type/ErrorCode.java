de7da04a24778db2b4ecef32d0a0c67a1a1bf74a
Fangsangik <hwangsangik@gmail.com>
2024-11-05 17:59:27 +0900

    refactor : "ErrorCode" - ErrorCode 수정

--- src/main/java/com/example/dailyschedule/error/type/ErrorCode.java
+++ src/main/java/com/example/dailyschedule/error/type/ErrorCode.java
@@ -13,12 +13,13 @@ public enum ErrorCode {
 
     NOT_FOUND("조회에 실패했습니다.", 404),
     ID_NOT_FOUND("존재하지 않는 ID 입니다.", 404),
+    DATE_NOT_FOUND("해당 날짜를 찾을 수 없습니다.", 404),
+    INVALID_MEMBER_INFO("존재하지 않는 회원입니다.", 404),
 
     PASSWORD_INCORRECT("비밀번호가 일치하지 않습니다.", 401),
     ID_INCORRECT("ID 값이 일치하지 않습니다.", 401),
 
-    INTERNAL_SERVER_ERROR("서버 에러 입니다, 서버팀에 연락 주세요!!", 500)
-    ;
+    INTERNAL_SERVER_ERROR("서버 에러 입니다, 서버팀에 연락 주세요!!", 500);
 
     private final String message;
     private final int status;
