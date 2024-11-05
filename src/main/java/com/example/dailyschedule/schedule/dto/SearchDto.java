54fad5ebf0ba40436a8f4ac3a623dbc88b8aa10c
Fangsangik <hwangsangik@gmail.com>
2024-11-05 18:13:33 +0900

    refactor : "Builder.Default 추가"

--- src/main/java/com/example/dailyschedule/schedule/dto/SearchDto.java
+++ src/main/java/com/example/dailyschedule/schedule/dto/SearchDto.java
@@ -8,8 +8,11 @@ import lombok.Getter;
 @Getter
 @Builder
 public class SearchDto {
+    @Builder.Default
     private int page = 1;         // 기본 페이지 번호
+    @Builder.Default
     private int pageSize = 10;    // 기본 페이지 크기
+    @Builder.Default
     private int recordSize = 10;  // 페이지당 기본 출력 개수
 
     public SearchDto() {
