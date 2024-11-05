fd19abde029850b5e8d99e4aa74fa7e876c40110
Fangsangik <hwangsangik@gmail.com>
2024-11-05 15:14:42 +0900

    refactor : "DeleteScheduleRequest" - annotation 추가

--- src/main/java/com/example/dailyschedule/schedule/dto/DeleteScheduleRequest.java
+++ src/main/java/com/example/dailyschedule/schedule/dto/DeleteScheduleRequest.java
@@ -1,8 +1,15 @@
 package com.example.dailyschedule.schedule.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+//DeletedDtp
 public class DeleteScheduleRequest {
     private Long id;
     private String password;
