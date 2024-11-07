963b87183d3acf5e8a003de971d22191de7e46a0
Fangsangik <hwangsangik@gmail.com>
2024-11-07 19:34:29 +0900

    refactor : "GlobalExceptionHandler" - {}제거

--- src/main/java/com/example/dailyschedule/error/GlobalExceptionHandler.java
+++ src/main/java/com/example/dailyschedule/error/GlobalExceptionHandler.java
@@ -9,13 +9,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler({CustomException.class})
+    @ExceptionHandler(CustomException.class)
     protected ResponseEntity<ErrorDto> handleCustomException(CustomException ex) {
         ErrorDto errorDto = new ErrorDto(ex.getErrorCode().getMessage(), ex.getErrorCode().getStatus());
         return new ResponseEntity<>(errorDto, HttpStatus.valueOf(ex.getErrorCode().getStatus()));
     }
 
-    @ExceptionHandler({Exception.class})
+    @ExceptionHandler(Exception.class)
     protected ResponseEntity<ErrorDto> handleServerException(Exception ex) {
         ErrorDto errorDto = new ErrorDto("서버 내부 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR.value());
         return new ResponseEntity<>(errorDto, HttpStatus.INTERNAL_SERVER_ERROR);
